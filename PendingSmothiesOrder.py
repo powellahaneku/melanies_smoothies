4d1b81e458409849de9534530957023fe44cccd9
Powell Ahaneku <136954284+powellahaneku@users.noreply.github.com>
2026-01-30 16:36:58 -0500

    Refactor Snowflake connection in PendingSmothiesOrder
    Updated Snowflake connection method and removed unused import.

---
+++ PendingSmothiesOrder.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title(f"Customize Your Smoothie!:cup_with_straw:")
+st.write(
+  """
+  Choose the fruits you want in your **custin Smoothie**!.
+  """
+)
+
+
+
+
+name_on_order = st.text_input('Name on Smoothie:')
+st.write("The name on your Smoothie will be:", name_on_order)
+
+# Snowflake connection
+cnx = st.connection("snowflake")
+session = cnx.session()
+my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+
+editable_df = st.data_editor(my_dataframe)
