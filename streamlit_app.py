db50effec41284a77178aa644a7708782dde3ff8
Powell Ahaneku <136954284+powellahaneku@users.noreply.github.com>
2026-01-30 16:54:50 -0500

    Modify dataframe conversion and API request
    Comment out the conversion of Snowpark dataframe to pandas and adjust the API request to use the search value.

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ st.dataframe(data=my_dataframe, use_container_width=True)
 
 
 #Convert the snowpark dataframe to a pandas dataframe
-pd_df = my_dataframe.to_pandas()
+#pd_df = my_dataframe.to_pandas()
 
 
 
@@ -38,10 +38,10 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
       
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df = st.dataframe(smoothiefroot_response.json(),use_container_width=True)
 
 
