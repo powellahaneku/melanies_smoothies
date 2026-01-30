1e14279c2e00e28692bc32add4eb9080454436b7
Powell Ahaneku <136954284+powellahaneku@users.noreply.github.com>
2026-01-30 17:16:13 -0500

    Convert DataFrame to pandas format for Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,7 @@ session = cnx.session() #get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
+pd_df = my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
