0086f8f827d7e354ebe80d5c0ef77c91713e3eae
Powell Ahaneku <136954284+powellahaneku@users.noreply.github.com>
2026-01-30 14:06:19 -0500

    Modify Snowflake session connection in streamlit_app
    Updated connection method to Snowflake and commented out the previous session import.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -17,8 +17,8 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write("The name on your Smoothie will be:", name_on_order)
 
-
-session = get_active_session()
+cnx = st.connection("snowflake") # for SniS version (Streamlit not in Snowflake)
+session = cnx.session() #get_active_session() 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
